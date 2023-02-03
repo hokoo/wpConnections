aea2547a64167a0467b9cfe0ee7904687db2c3ee
Igor Tron <igor_tron@epam.com>
2023-02-03 17:13:12 +0100

    Exception methods got added

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -3,5 +3,11 @@
 namespace iTRON\wpConnections\Exceptions;
 
 class Exception extends \Exception {
+    public function setMessage( string $message, bool $append = true ) {
+        $this->message = $append ? $this->message . $message : $message;
+    }
 
+    public function setCode( int $code ) {
+        $this->code = $code;
+    }
 }
