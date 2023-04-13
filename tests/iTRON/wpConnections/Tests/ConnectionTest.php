828377d790ac7048681da87e1aa45edbb12f4490
Igor Tron <public@itron.pro>
2023-04-13 14:08:37 +0200

    abstract connection fixed + test added

---
+++ tests/iTRON/wpConnections/Tests/ConnectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace iTRON\wpConnections\Tests;
+
+use iTRON\wpConnections\Connection;
+use PHPUnit\Framework\TestCase;
+
+class ConnectionTest extends TestCase {
+	public function testCreateConnection(){
+		$connection = new Connection( new \iTRON\wpConnections\Query\Connection( 1, 2 ) );
+		self::assertEquals( 1, $connection->from );
+		self::assertEquals( 2, $connection->to );
+	}
+}
