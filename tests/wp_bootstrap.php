f9fa788faa827470c0a4e75708ad92e0edfa2b3d
Igor Tron <public@itron.pro>
2024-09-07 19:52:22 +0400

    bootstrap got fixed

--- tests/wp_bootstrap.php
+++ tests/wp_bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 $test_root = getenv( 'WP_TESTS_DIR' ) ? : dirname( __FILE__ ) . '/../wordpress-develop/tests/phpunit';
 
-require $test_root . '/includes/functions.php';
-
-require dirname( __FILE__ ) . '/include.php';
-
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../plugin.php';
 }
