2e378cb7fe7cc5759b66bcd34fed6e06bd50e4b5
Igor Tron <public@itron.pro>
2025-04-20 21:55:10 +0400

    php 7.4 fix

--- src/Capabilities.php
+++ src/Capabilities.php
@@ -4,13 +4,12 @@ namespace iTRON\wpConnections;
 
 class Capabilities
 {
-    public const DEFAULT = 'manage_options';
-
     private array $capabilities = [];
+    private string $defaultCap = '';
 
-    public function __construct(private string $defaultCap = '')
+    public function __construct(string $defaultCap = '')
     {
-        $this->defaultCap = $defaultCap ?: self::DEFAULT;
+        $this->defaultCap = $defaultCap ?: 'manage_options';
     }
 
     public function __get(string $capability): string
