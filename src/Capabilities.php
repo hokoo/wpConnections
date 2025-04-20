d95e154b91969fad99d66641ac5af2c177cbfc0a
Igor Tron <public@itron.pro>
2025-04-20 21:46:44 +0400

    filtering default caps for client

--- src/Capabilities.php
+++ src/Capabilities.php
@@ -8,9 +8,14 @@ class Capabilities
 
     private array $capabilities = [];
 
+    public function __construct(private string $defaultCap = '')
+    {
+        $this->defaultCap = $defaultCap ?: self::DEFAULT;
+    }
+
     public function __get(string $capability): string
     {
-        return $this->capabilities[ $capability ] ?? self::DEFAULT;
+        return $this->capabilities[ $capability ] ?? $this->defaultCap;
     }
 
     public function __set(string $capability, string $value): void
